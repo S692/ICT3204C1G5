fa0051f01c247853c14b38c89957066f2237f7dc
S692 <2002692@sit.singaporetech.edu.sg>
2022-10-11 20:19:46 +0800

    Working redis and PE

--- setup.sh
+++ setup.sh
@@ -1,24 +1,53 @@
 #!/bin/bash
-whoami
+
 echo "Installing dependencies..."
 #
-sudo apt update
-sudo apt install build-essential
+sudo apt update -y
+sudo apt install build-essential -y
+sudo apt install curl -y
 sudo apt-get -y install net-tools
 sudo apt-get install -y iputils-ping
 sudo apt-get -y install software-properties-common
 sudo apt install -y less
 sudo apt install -y unzip
 sudo apt install -y nano
+sudo apt install -y rsyslog
+wget https://raw.githubusercontent.com/S692/vagrant-A1/main/rsyslogConfig.py
+python3 rsyslogConfig.py
+sudo service rsyslog start
+sudo apt install iproute2 -y
+sudo apt -y install make gcc
 
+mkdir ~/redis && cd ~/redis
+cd ~/redis
+wget http://download.redis.io/releases/redis-4.0.0.tar.gz
+tar xvzf redis-4.0.0.tar.gz
+cd /root/redis/redis-4.0.0
+dpkg --configure -a
+make
+wget https://raw.githubusercontent.com/S692/vagrant-A1/main/redisConf.py
+python3 redisConf.py
+
+# Create new user... using the sucky useradd because idk how automate adduser's password
+sudo useradd -p $(openssl passwd -1 123) resch
+mkdir /home/resch
+cp -rT /etc/skel /home/resch
+chown -R resch:resch /home/resch
+cd /home/resch
+mkdir .ssh
+ssh-keygen -f ./.ssh/id_rsa -N ""
+chown -R resch:resch /home/resch/.ssh
+
+
+# --------------------------------------------------------------------
 # for cronjob
 sudo mkdir /var/research
 sudo chmod 744 /var/research
 sudo touch /var/research/research1.pdf
 sudo touch /var/research/research2.pdf
+sudo chown -R resch:resch /var/research
+
 sudo echo "* *   * * *   root   cd /var/research && tar cf /var/backups/backup.tgz *" >> /etc/crontab
 sudo service cron start
-# create user researcher as resch
-sudo useradd -p $(openssl passwd -1 1) resch
-sudo chown resch:resch /var/research
 
+# ------------------------------------------------------------------------
