22a53177e36ae07f89069a7bad78cbc96ee35536
S692 <74851072+S692@users.noreply.github.com>
2022-10-11 15:32:44 +0800

    Add files via upload

---
+++ rsyslogConfig.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+with open('/etc/rsyslog.d/50-default.conf', 'r') as file:
+    data = file.read()
+
+data = data.replace('#cron.*', 'cron.*')
+data = data.replace('#user.*', 'user.*')
+data = data.replace('#mail.info', 'mail.info')
+
+with open('/etc/rsyslog.d/50-default.conf', 'w') as file:
+    file.write(data)
