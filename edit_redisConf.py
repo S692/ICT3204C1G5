829aceb2adb8c14a86e2a16efc2d46d259130690
S692 <74851072+S692@users.noreply.github.com>
2022-10-11 12:30:03 +0800

    Update edit_redisConf.py

--- edit_redisConf.py
+++ edit_redisConf.py
@@ -1,6 +1,6 @@
 import fileinput
 
-fp = r'/root/redis_src/redis-3.2.11/redis.conf'
+fp = r'/root/redis_src/redis-4.0.0/redis.conf'
 with fileinput.FileInput(fp, inplace=True, backup='.bak') as file:
     for line in file:
         print(line.replace('bind 127.0.0.1', '\# bind 127.0.0.1'), end='')
