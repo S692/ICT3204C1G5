76271da8e2dc11e91aebb5ff6a7254576683d406
S692 <74851072+S692@users.noreply.github.com>
2022-10-11 12:56:21 +0800

    Update edit_redisConf.py

--- edit_redisConf.py
+++ edit_redisConf.py
@@ -3,7 +3,7 @@ import fileinput
 fp = r'/root/redis/redis-4.0.0/redis.conf'
 with fileinput.FileInput(fp, inplace=True, backup='.bak') as file:
     for line in file:
-        print(line.replace('bind 127.0.0.1', '\# bind 127.0.0.1'), end='')
+        print(line.replace('bind 127.0.0.1', '# bind 127.0.0.1'), end='')
 with fileinput.FileInput(fp, inplace=True, backup='.bak') as file:
     for line in file:
         print(line.replace('protected-mode yes', 'protected-mode no'), end='')
