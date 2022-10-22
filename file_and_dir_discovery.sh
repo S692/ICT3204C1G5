6a2978d5cb8d61f0ea7ce65956af37c26f6495c0
Anne Tan <SIT2002716@gmail.com>
2022-10-22 19:46:28 +0800

    fndd bash script draft v2

--- file_and_dir_discovery.sh
+++ file_and_dir_discovery.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #assuming attacker is already aware of the valuable research data hosted on the target server. hence it targets .docx and .xslx files
+echo -e "Start File and Directory Discovery...\n"
 
+echo -e "Errors (if any): \n"
 rm -f fndd.txt
 
 #common file extensions; png, jpg, mp3, txt, pdf, doc, xls
 echo -e "----------------------------common file extensions----------------------------\n" > fndd.txt
-echo -e "-Extension Type 01: Documents \n" > fndd.txt
+echo -e "\nExtension Type 01: Documents \n" > fndd.txt
 find /home/osboxes -name "*.doc" -or -name "*.docx" -or -name "*.pdf" -printf "%a %p \n" >> fndd.txt
-echo -e "\n-Extension Type 02: Excel sheets \n" > fndd.txt
+echo -e "\nExtension Type 02: Excel sheets \n" > fndd.txt
 find /home/osboxes -name "*.xls" -or -name "*.xlsx" -or -name "*.csv"  -printf "%a %p \n" >> fndd.txt
-echo -e "\n-Extension Type 03: Media \n" > fndd.txt
+echo -e "\nExtension Type 03: Media \n" > fndd.txt
 find /home/osboxes -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.mp3"  -printf "%a %p \n" >> fndd.txt
 #ls -la - R $PWD/* grep \.pdf$ | grep \.png$ >> fndd.txt
-
 #$ls -la -R $PWD/* | grep \.pdf$ | grep \.mp3$ 
 
 # #for the following /home/<user>/ dir discovery, it is assumed that the user list has already been previously discovered by the attacker
@@ -31,13 +32,17 @@ ls -la -R /var/backup >> fndd.txt
 echo -e "-----------------------------.ssh backup key ls----------------------------\n" >> fndd.txt
 ls -la -R /home/rsch/.ssh/ >> fndd.txt
 
-#check if /etc/shaodow and /etc/passwd exists
-echo -e "----------------------------shadow and config with root permission---------------------------\n" >> fndd.txt
+#check location and if /etc/shaodow and /etc/passwd exists
+echo -e "----------------------------shadow files and config with root permission---------------------------\n" >> fndd.txt
 #ls -lad /etc/shadow >> fndd.txt
-find /etc/ -group shadow -ls && find /etc/ -user root -name "*.conf" -ls >> fndd.txt
+find /etc/ -group shadow -printf "%a %p \n" >> fndd.txt
+find /etc/ -user root -name "*.conf" -printf "%a %p \n" >> fndd.txt
 
 
 #hidden folders
-echo -e "-------------------.ssh backup key ls------------------\n" >> fndd.txt
+echo -e "----------------------------other hidden file/folder----------------------------\n" >> fndd.txt
 find . -name ".*" -maxdepth 1 2> /dev/null >> fndd.txt
+
+echo -e "\nFile and Directory Discovery Complete...\n"
+
 
