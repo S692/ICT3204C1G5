ae384598a246c1f21ee26670700d30a12f6c2002
Anne Tan <SIT2002716@gmail.com>
2022-10-22 20:22:30 +0800

    fndd bash script draft v2.1
    minor changes and syntax correction
    modified directory paths custom to target

--- file_and_dir_discovery.sh
+++ file_and_dir_discovery.sh
@@ -3,16 +3,18 @@
 echo -e "Start File and Directory Discovery...\n"
 
 echo -e "Errors (if any): \n"
+echo -e "$PWD \n"
 rm -f fndd.txt
 
 #common file extensions; png, jpg, mp3, txt, pdf, doc, xls
 echo -e "----------------------------common file extensions----------------------------\n" > fndd.txt
 echo -e "\nExtension Type 01: Documents \n" > fndd.txt
-find /home/osboxes -name "*.doc" -or -name "*.docx" -or -name "*.pdf" -printf "%a %p \n" >> fndd.txt
+find /home/resch "*.doc" -or -name "*.docx" -or -name "*.pdf" -printf "%a %p \n" >> fndd.txt
 echo -e "\nExtension Type 02: Excel sheets \n" > fndd.txt
-find /home/osboxes -name "*.xls" -or -name "*.xlsx" -or -name "*.csv"  -printf "%a %p \n" >> fndd.txt
+find /home/resch -name "*.xls" -or -name "*.xlsx" -or -name "*.csv" -printf "%a %p \n" >> fndd.txt
 echo -e "\nExtension Type 03: Media \n" > fndd.txt
-find /home/osboxes -name "*.png" -or -name "*.jpg" -or -name "*.jpeg" -or -name "*.mp3"  -printf "%a %p \n" >> fndd.txt
+find /home/resch -name "*.png" -or -name "*.jpg" -printf "%a %p \n" >> fndd.txt
+find /home/resch -name "*.jpeg" -or -name "*.mp3" -printf "%a %p \n" >> fndd.txt
 #ls -la - R $PWD/* grep \.pdf$ | grep \.png$ >> fndd.txt
 #$ls -la -R $PWD/* | grep \.pdf$ | grep \.mp3$ 
 
@@ -26,22 +28,22 @@ find ~/ -name ".bash*" -or -name ".profile*" -printf "%a %p \n" >> fndd.txt
 
 #backup folder 
 echo -e "----------------------------backup----------------------------\n" >> fndd.txt
-ls -la -R /var/backup >> fndd.txt
+ls -la -R /var/backups/* >> fndd.txt
 
 #.ssh folder, if there are any other ssh keys than the one planted for initial access
 echo -e "-----------------------------.ssh backup key ls----------------------------\n" >> fndd.txt
-ls -la -R /home/rsch/.ssh/ >> fndd.txt
+ls -la -R ~/.ssh* >> fndd.txt
 
 #check location and if /etc/shaodow and /etc/passwd exists
 echo -e "----------------------------shadow files and config with root permission---------------------------\n" >> fndd.txt
 #ls -lad /etc/shadow >> fndd.txt
 find /etc/ -group shadow -printf "%a %p \n" >> fndd.txt
 find /etc/ -user root -name "*.conf" -printf "%a %p \n" >> fndd.txt
-
+find /etc/ -user resch -name "*.conf" -printf "%a %p \n" >> fndd.txt
 
 #hidden folders
-echo -e "----------------------------other hidden file/folder----------------------------\n" >> fndd.txt
-find . -name ".*" -maxdepth 1 2> /dev/null >> fndd.txt
+echo -e "----------------------------all hidden file/folder----------------------------\n" >> fndd.txt
+find / -name ".*" -maxdepth 5 2> /dev/null >> fndd.txt
 
 echo -e "\nFile and Directory Discovery Complete...\n"
 
