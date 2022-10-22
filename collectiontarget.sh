26510c12a2daa9e565bd1e9c09a68a34bb67da30
suhail97s <suhail1997.ms@gmail.com>
2022-10-22 20:55:53 +0800

    adjust cronjob

--- collectiontarget.sh
+++ collectiontarget.sh
@@ -3,5 +3,5 @@
 # for copying files and directories from a list.txt that need connect with anne
 cat discovery.txt | xargs -I % rsync -a % /destination/
 # to be added into crontab that will run every 2 minutes:
-2 * * * * * cat discovery.txt | xargs -I % rsync -a % /destination/ >> /etc/crontab
+2 * * * * cat discovery.txt | xargs -I % rsync -a % /destination/ >> /etc/crontab
 # For Mail: cp -r ~/Maildir
