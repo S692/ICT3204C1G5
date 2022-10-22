570067eaa66d342da90529a0daeabdc9e414925a
suhail97s <suhail1997.ms@gmail.com>
2022-10-23 00:03:32 +0800

    Update collectiontarget.sh

--- collectiontarget.sh
+++ collectiontarget.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # for copying files and directories from a list.txt that need connect with anne
-cat discovery.txt | xargs -I % rsync -a % /destination/
+mkdir /home/destination
+cat /home/resch/fndd.txt | xargs -I % rsync -a % /home/destination
 # to be added into crontab that will run every 2 minutes:
-2 * * * * cat discovery.txt | xargs -I % rsync -a % /destination/ >> /etc/crontab
+2 * * * * cat /home/resch/fndd.txt | xargs -I % rsync -a % /home/destination/ >> /etc/crontab
 # For Mail: cp -r ~/Maildir
