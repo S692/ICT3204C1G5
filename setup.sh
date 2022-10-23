7f20008a87e916bfd24ae27b08db77c19f6111ba
Anne Tan <74164071+quadrupleflip@users.noreply.github.com>
2022-10-23 11:58:04 +0800

    Changed directory for fndd dummy files

--- setup.sh
+++ setup.sh
@@ -125,8 +125,8 @@ wget https://github.com/S692/ossas/blob/main/dummy-files-for-email/1666459259.Vc
 wget https://github.com/S692/ossas/blob/main/dummy-files-for-email/1666459365.VceI9947M833420.target
 
 # to plant dummy files in target for file and folder discovery 
-mkdir /home/resch/3204ResearchMaterials
-cd /home/resch/3204ResearchMaterials
+mkdir /var/research/3204ResearchMaterials
+cd /home/research/3204ResearchMaterials
 wget https://github.com/S692/ossas/blob/main/dummy-files-for-file_and_dir_discovery/3204_security_analytics.txt
 wget https://github.com/S692/ossas/blob/main/dummy-files-for-file_and_dir_discovery/3204lab-test-results-1.xlsx
 wget https://github.com/S692/ossas/blob/main/dummy-files-for-file_and_dir_discovery/Details-of-Deliverable-1_ICT3203.pdf
