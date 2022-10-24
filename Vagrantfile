14534908cc5345372e6769ec3876e71261ac658d
Shahidah Imran <shahidahnur27@yahoo.com>
2022-10-25 06:33:16 +0800

    Testing.

--- Vagrantfile
+++ Vagrantfile
@@ -16,10 +16,6 @@ Vagrant.configure("2") do |config|
       d.remains_running = true
       d.name = "elk"
     end
-	
-	elk.trigger.after :up, :provision do |trigger|
-		trigger.run = {inline: "./trigger.sh"}
-	end
   end
 
   # Target container
--- trigger.sh
+++ trigger.sh
@@ -4,17 +4,14 @@ echo ">>>>  Right before SG initialization <<<<"
 # use while loop to check if elasticsearch is running 
 while true
 do
-    netstat -uplnt | grep :9200 | grep LISTEN > /dev/null
-    verifier=$?
-    if [ 0 = $verifier ]
-        then
-            echo "Starting metricbeat"
+    if [ps -A | grep -i elasticsearch]
+		echo "Starting metricbeat"
             metricbeat setup -e
 			service metricbeat start
             break
-        else
-            echo "ES is not running yet"
-            sleep 5
+    else
+        echo "ES is not running yet"
+        sleep 5
     fi
 done
 
